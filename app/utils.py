9128f7ddec1f0b5569442a7bf2d5f2c52faeae87
son25 <thaisoncbq@gmail.com>
2025-09-06 16:22:06 +0700

    update bcrypt problem and utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,22 @@
-from passlib.context import CryptContext
+# from passlib.context import CryptContext
 
-pwd_context = CryptContext(schemes="bcrypt", deprecated="auto")
+# pwd_context = CryptContext(schemes="bcrypt", deprecated="auto")
 
-def hash(password: str):
-    return pwd_context.hash(password)
+# def hash(password: str):
+#     return pwd_context.hash(password)
 
-def verify(password: str, hashed_password: str):
-    return pwd_context.verify(password, hashed_password)
+# def verify(password: str, hashed_password: str):
+#     return pwd_context.verify(password, hashed_password)
+
+import bcrypt
+
+def hash(password: str) -> str:
+    pwd_bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(pwd_bytes, salt)
+    return hashed_password.decode('utf-8')
+
+def verify(password: str, hashed_password: str) -> bool:
+    password_bytes = password.encode('utf-8')
+    hashed_bytes = hashed_password.encode('utf-8')
+    return bcrypt.checkpw(password_bytes, hashed_bytes)
